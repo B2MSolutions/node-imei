c046c45f5391d9be605c8ea2c5f72939e7bcc5e6
roylines <roy@roylines.co.uk>
2014-02-03 14:58:08 +0000

    implemented isValid and next

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var checkdigit = require('checkdigit');
+
 var imei = {};
+
+imei.isValid = function(i) {
+  return i.length === 15 && checkdigit.mod10.isValid(i);
+};
+
+imei.next = function(prev, done) {
+  if(!imei.isValid(prev)) {
+    return done('invalid imei');
+  }
+
+  var withoutLuhn = prev.substr(0, 14);
+  var nextWithoutLuhn = parseInt(withoutLuhn, 10) + 1;
+  var next = checkdigit.mod10.apply(nextWithoutLuhn.toString());
+
+  return done(null, next);
+};
 
 module.exports = imei;
--- test/imei_test.js
+++ test/imei_test.js
@@ -1,5 +1,29 @@
+var assert = require('assert'),
+  imei = require('../index.js');
+
 describe('imei', function() {
-  it('should pass this test', function(done) {
-    return done();
+  describe('isValid', function() {
+    it('should return true for valid imeis', function() {
+      assert(imei.isValid('352099001761481'));
+    });
+    it('should return false for valid imeis', function() {
+      assert(!imei.isValid('352099001761482')); // wrong luhn
+      assert(!imei.isValid('52099001761484'));  // too short
+    });
+  });
+
+  describe('next', function() {
+    it('should return next one for valid imei', function(done) {
+      imei.next('352099001761481', function(e, next) {
+        assert.equal(next, '352099001761499');
+        return done(e);
+      });
+    });
+    it('should return error for invalid imei', function(done) {
+      imei.next('352099001761482', function(e, next) {
+        assert.equal(e, 'invalid imei');
+        return done();
+      });
+    });
   });
 });
