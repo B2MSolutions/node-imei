83e29649fca0a0c41c84161a9274c60f34bac171
Cody Hazelwood <codyhazelwood@gmail.com>
2016-07-26 20:49:37 -0500

    Allow parameters to be numbers or strings (since IMEIs are numbers).

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ var checkdigit = require('checkdigit');
 var imei = {};
 
 imei.isValid = function(i) {
-  return i.length === 15 && checkdigit.mod10.isValid(i);
+  var str = i.toString();
+  return str.length === 15 && checkdigit.mod10.isValid(str);
 };
 
 imei.next = function(prev, done) {
+  var prevStr = prev.toString();
   if(!imei.isValid(prev)) {
     return done('invalid imei');
   }
 
-  var serialNumber = prev.substr(8, 6);
+  var serialNumber = prevStr.substr(8, 6);
   if(serialNumber === '999999') {
     return done('no more IMEIs in TAC range');
   }
 
-  var withoutLuhn = prev.substr(0, 14);
+  var withoutLuhn = prevStr.substr(0, 14);
   var nextWithoutLuhn = parseInt(withoutLuhn, 10) + 1;
   var next = checkdigit.mod10.apply(nextWithoutLuhn.toString());
 
